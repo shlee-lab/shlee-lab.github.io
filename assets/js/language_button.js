895027750030d1f64130c7e889e5caab80c677e3
shlee-lab <36615016+shlee-lab@users.noreply.github.com>
2026-07-08 16:26:08 +0000

    Deploying to gh-pages from @ shlee-lab/shlee-lab.github.io@d55c8618c870927b4f18504310c1a0c675372421 🚀

--- assets/js/language_button.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){document.getElementById("toggleLanguage").addEventListener("click",function(){var e=document.querySelectorAll(".english"),t=document.querySelectorAll(".korean");e.forEach(e=>e.classList.toggle("active")),t.forEach(e=>e.classList.toggle("active"))})});
